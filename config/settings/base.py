a80cc6f76e2a10b9e7d70497ddba68fd5914a002
fabianrios <faben02@gmail.com>
2018-08-15 12:32:14 +0200

    CORS done

--- config/settings/base.py
+++ config/settings/base.py
@@ -71,6 +71,7 @@ DJANGO_APPS = [
 THIRD_PARTY_APPS = [
     'nltk',
     'tweepy',
+    'corsheaders',
     'requests',
     'crispy_forms',
     'allauth',
@@ -140,12 +141,19 @@ AUTH_PASSWORD_VALIDATORS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+#CORS
+# ------------------------------------------------------------------------------
+# https://github.com/ottoyiu/django-cors-headers
+ALLOWED_HOSTS = ["*"]
+CORS_ORIGIN_ALLOW_ALL = True
 
 # STATIC
 # ------------------------------------------------------------------------------
